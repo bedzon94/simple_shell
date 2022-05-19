776ba600a0ba78701d6a577cda95d7dfdd1edb6e
Abayie <jonathanboahene@gmail.com>
2022-05-19 07:12:24 -0700

    Noris main.c file

--- main.c
+++ main.c
@@ -1,73 +1,69 @@
 #include "shell.h"
-
+void sig_handler(int sig);
+int execute(char **args, char **front);
 /**
- * free_data - frees data structure
- *
- * @datash: data structure
- * Return: no return
+ * sig_handler - Prints a new prompt upon a signal.
+ * @sig: The signal.
  */
-void free_data(data_shell *datash)
+void sig_handler(int sig)
 {
-	unsigned int i;
-
-	for (i = 0; datash->_environ[i]; i++)
-	{
-		free(datash->_environ[i]);
-	}
-
-	free(datash->_environ);
-	free(datash->pid);
+    char *new_prompt = "\n$ ";
+    (void)sig;
+    signal(SIGINT, sig_handler);
+    write(STDIN_FILENO, new_prompt, 3);
 }
-
 /**
- * set_data - Initialize data structure
+ * execute - Executes a command in a child process.
+ * @args: An array of arguments.
+ * @front: A double pointer to the beginning of args.
  *
- * @datash: data structure
- * @av: argument vector
- * Return: no return
+ * Return: If an error occurs - a corresponding error code.
+ *         O/w - The exit value of the last executed command.
  */
-void set_data(data_shell *datash, char **av)
+int execute(char **args, char **front)
 {
-	unsigned int i;
-
-	datash->av = av;
-	datash->input = NULL;
-	datash->args = NULL;
-	datash->status = 0;
-	datash->counter = 1;
-
-	for (i = 0; environ[i]; i++)
-		;
-
-	datash->_environ = malloc(sizeof(char *) * (i + 1));
-
-	for (i = 0; environ[i]; i++)
-	{
-		datash->_environ[i] = _strdup(environ[i]);
-	}
-
-	datash->_environ[i] = NULL;
-	datash->pid = aux_itoa(getpid());
-}
-
-/**
- * main - Entry point
- *
- * @ac: argument count
- * @av: argument vector
- *
- * Return: 0 on success.
- */
-int main(int ac, char **av)
-{
-	data_shell datash;
-	(void) ac;
-
-	signal(SIGINT, get_sigint);
-	set_data(&datash, av);
-	shell_loop(&datash);
-	free_data(&datash);
-	if (datash.status < 0)
-		return (255);
-	return (datash.status);
+    pid_t child_pid;
+    int status, flag = 0, ret = 0;
+    char *command = args[0];
+    if (command[0] != '/' && command[0] != '.')
+    {
+        flag = 1;
+        command = get_location(command);
+    }
+    if (!command || (access(command, F_OK) == -1))
+    {
+        if (errno == EACCES)
+            ret = (create_error(args, 126));
+        else
+            ret = (create_error(args, 127));
+    }
+    else
+    {
+        child_pid = fork();
+        if (child_pid == -1)
+        {
+            if (flag)
+                free(command);
+            perror("Error child:");
+            return (1);
+        }
+        if (child_pid == 0)
+        {
+            execve(command, args, environ);
+            if (errno == EACCES)
+                ret = (create_error(args, 126));
+            free_env();
+            free_args(args, front);
+            free_alias_list(aliases);
+            _exit(ret);
+        }
+        else
+        {
+            wait(&status);
+            ret = WEXITSTATUS(status);
+        }
+    }
+    if (flag)
+        free(command);
+    return (ret);
 }
