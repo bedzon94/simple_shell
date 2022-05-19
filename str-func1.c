233283d2a6feaa845fb23fbc0f6eeb4f15ae63af
Norris <bedzon@ieee.org>
2022-05-19 16:26:49 -0400

    str-func1.c

--- str-func1.c
+++ str-func1.c
@@ -76,6 +76,7 @@ char *_strcat(char *dest, const char *src)
  *
  * Return: Pointer to destination string.
  */
+
 char *_strncat(char *dest, const char *src, size_t n)
 {
 	size_t dest_len = _strlen(dest);
